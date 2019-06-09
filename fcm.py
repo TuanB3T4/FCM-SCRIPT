23708e7e14bfefe8b6673f1d47f6d1197baa874c
B3t4 <49000749+TuanB3T4@users.noreply.github.com>
2019-06-10 04:58:50 +0700

    Update fcm.py

--- fcm.py
+++ fcm.py
@@ -133,7 +133,7 @@ messagescript7 = """ [=]</font>
 
 messagescript8 = imagelink
 
-messagescript9 = """ width=900px height=530px>
+messagescript9 = """ width=840px height=420px>
 <body onload="init()"></body>
 <body>
 <div id="bulle"></div>"""
@@ -182,7 +182,6 @@ return
 
 </script><font face="Orbitron" size="1"><blink><span style="color: rgb(255, 255, 255);"><b><font color=lime size=4></font></b></u></blink><br></font></b>
 
-<br><br><br>
 <marquee behavior="scroll" direction="left" scrollamount="90" scrolldelay="40" width="100%">
 <font color="green">___________________________________________________________</font></marquee>
 <div style="text-shadow: 0px 0px 10px green;"><span style="color: white;"><font face="transformers"><b>Thanks To : <marquee scrollamount="5" direction="left" width="50%"><span style="color: red;"> <span style="color: red;">[ B3T4 ]<span style="color: red;"></b></marquee></font></div><script type="text/rocketscript">/*<![CDATA[*/new TypingText(document.getElementById("message"), 90, function(i){ var ar= new Array("_", " ", "_", " "); return "" +ar[i.length % ar.length]; });//Type out examples:TypingText.runAll();/*]]>*/</script>
@@ -240,7 +239,7 @@ fo.write(messagescript14)
 fo.write(messagescript15)
 fo.write(messagescript16)
 
-print "Script Berhasil Di buat!"
-print "TERIMAKASIH TELAH MENGGUNAKAN TOOLS INI"
+print "\033[91mScript Berhasil Di buat!"
+print "\033[91mTERIMAKASIH TELAH MENGGUNAKAN TOOLS INI"
 
 fo.close()
